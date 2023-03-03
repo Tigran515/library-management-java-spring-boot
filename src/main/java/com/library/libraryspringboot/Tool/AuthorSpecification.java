9f8730998c659dab548502ada8e234aeb0bd811a
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-03-03 22:48:31 +0400

    ci 💚(search author) searching field added

--- src/main/java/com/library/libraryspringboot/Tool/AuthorSpecification.java
+++ src/main/java/com/library/libraryspringboot/Tool/AuthorSpecification.java
@@ -8,7 +8,7 @@ import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 public class AuthorSpecification implements Specification<Author> { //@TODO: read about Specification<T>
-    private String search;
+    private final String search;
 
     public AuthorSpecification(String search) {
         this.search = search;
@@ -19,7 +19,8 @@ public class AuthorSpecification implements Specification<Author> { //@TODO: rea
         String likePattern = "%" + search + "%";
         return builder.or(
                 builder.like(builder.lower(root.get("name")), likePattern),
-                builder.like(builder.lower(root.get("lname")), likePattern)
+                builder.like(builder.lower(root.get("lname")), likePattern),
+                builder.like(builder.lower(root.get("sname")), likePattern)
         );
     }
 
