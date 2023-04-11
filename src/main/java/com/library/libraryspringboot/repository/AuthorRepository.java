e82850c3b04005f367af8578ac9c9efe097c41d9
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-04-11 12:04:40 +0400

    ♻ (findAll) changed return type to Page

--- src/main/java/com/library/libraryspringboot/repository/AuthorRepository.java
+++ src/main/java/com/library/libraryspringboot/repository/AuthorRepository.java
@@ -1,16 +1,12 @@
 package com.library.libraryspringboot.repository;
 
 import com.library.libraryspringboot.entity.Author;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
-public interface AuthorRepository extends CrudRepository<Author, String> { //TODO: change to Integer
-    List<Author> findAll(Specification<Author> specification);
-
+public interface AuthorRepository extends JpaRepository<Author, String> {
+    Page<Author> findAll(final Pageable pageable);
     boolean existsAuthorByNameAndLnameAndSname(String name, String lname, String sname);
-
-    //    boolean existsAuthorByNameOrLnameOrSnameOrLname(String name, String lname, String sname);
-//    boolean existsAuthorByNameAndOrLnameAndOrSname(String name, String lname, String sname);
 }
