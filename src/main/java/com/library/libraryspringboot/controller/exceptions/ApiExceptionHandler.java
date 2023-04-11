173a867af2298e8fe34d87bfac25b33b2e3cc785
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-04-11 12:03:22 +0400

    ci 💚(custom exceptions) replaced custom exceptions with similar global ones

--- src/main/java/com/library/libraryspringboot/controller/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/library/libraryspringboot/controller/exceptions/ApiExceptionHandler.java
@@ -1,37 +1,39 @@
 package com.library.libraryspringboot.controller.exceptions;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler(value = ResourceNotFoundException.class) //NoSuchElementException
+    @ExceptionHandler(value = NoSuchElementException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex) {
+    public ErrorMessage noSuchElementException(NoSuchElementException ex) {
+        //add logger here
         return new ErrorMessage(HttpStatus.NOT_FOUND.value(),
                 new Date(),
                 ex.getMessage(),
                 "Resource Not Found");
     }
 
-    @ExceptionHandler(value = MethodInvalidArgumentException.class)
+    @ExceptionHandler(value = IllegalArgumentException .class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorMessage methodInvalidArgumentException(MethodInvalidArgumentException ex) { //@TODO remove , use JAVA's
+    public ErrorMessage methodInvalidArgumentException(IllegalArgumentException  ex) {
         return new ErrorMessage(HttpStatus.BAD_REQUEST.value(),
                 new Date(),
                 ex.getMessage(),
                 "User input arguments are not valid");
     }
 
-    @ExceptionHandler(value = DataDuplicationException.class) //DuplicateElementException
+    @ExceptionHandler(value = DuplicateKeyException.class) //DuplicateElementException is a custom exception, DuplicateKeyException is passed instead
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorMessage dataDuplicationException(DataDuplicationException ex) {
+    public ErrorMessage duplicateKeyException(DuplicateKeyException ex) {
         return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 new Date(),
                 ex.getMessage(),
