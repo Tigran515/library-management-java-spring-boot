991774ad999718caf5e067f6d34b07f066edeb8b
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-04-11 12:06:38 +0400

    ♻ (refactored) CRUD->Jpa Repository

--- src/main/java/com/library/libraryspringboot/repository/BookAuthorRepository.java
+++ src/main/java/com/library/libraryspringboot/repository/BookAuthorRepository.java
@@ -1,32 +1,27 @@
 package com.library.libraryspringboot.repository;
 
 import com.library.libraryspringboot.Tool.BookAuthorId;
-import com.library.libraryspringboot.entity.Author;
 import com.library.libraryspringboot.entity.Book;
 import com.library.libraryspringboot.entity.BookAuthor;
-import dto.BookAuthorDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BookAuthorRepository extends CrudRepository<BookAuthor, BookAuthorId> {
-    //    @EntityGraph(attributePaths = {"book"})
-//    Optional<CrossTable> findById(String id);
-//    List<BookAuthor> findByCrossTableIdBookId(Integer id);
-//    List<Author> findByBook_Id(Integer bookId);
-//    List<BookAuthor> findByBook_Id(Integer bookId); // uncomment
-//    BookAuthor searchBookAuthorByBookAuthorId(String bookId, String authorId);
-//    boolean existsBookAuthorByBookIdAndAuthorId(String bookId, String authorId);
-
+public interface BookAuthorRepository extends JpaRepository<BookAuthor, BookAuthorId> {
     List<BookAuthor> findAll(Specification<BookAuthor> specification);
+    Page<BookAuthor> findAll(final Pageable pageable);
 
-    //        @Query("SELECT ba FROM BookAuthor ba WHERE ba.bookAuthorId.bookId = ?1")
+    // Spring Data JPA Query Method written using the JPA Query Language (JPQL).
     @Query("SELECT ba FROM BookAuthor ba JOIN FETCH ba.bookAuthorId.authorId a WHERE ba.bookAuthorId.bookId = ?1")
     Optional<BookAuthor> findBookAuthorByBookId(Book id);
 
-    @Query("SELECT ba FROM BookAuthor ba JOIN FETCH ba.bookAuthorId.authorId a WHERE ba.bookAuthorId.bookId = ?1")
-    List<BookAuthor> findAllBookAuthorByBookId(Book id);
+    @Modifying //The @Modifying annotation is used to enhance the @Query annotation so that we can execute not only SELECT queries, but also INSERT, UPDATE, DELETE, and even DDL queries
+    @Query("DELETE FROM BookAuthor ba WHERE ba.bookAuthorId.bookId = ?1")
+    void deleteBookAuthorByBookId(Book id);
 }
