591c79234a23315b7d1e0c76231a0c28e82991bf
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-05-20 09:01:44 +0400

    💚 ci(methods): add Logger

--- src/main/java/com/library/libraryspringboot/service/AuthorServiceImpl.java
+++ src/main/java/com/library/libraryspringboot/service/AuthorServiceImpl.java
@@ -5,7 +5,8 @@ import com.library.libraryspringboot.entity.Author;
 import com.library.libraryspringboot.repository.AuthorRepository;
 import dto.AuthorDTO;
 import jakarta.validation.*;
-import jakarta.validation.constraints.NotBlank;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,6 +19,7 @@ import java.util.Optional;
 public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
     private final AuthorConverter authorConverter;
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorServiceImpl.class);
 
     public AuthorServiceImpl(AuthorRepository authorRepository, AuthorConverter authorConverter) {
         this.authorRepository = authorRepository;
@@ -25,36 +27,44 @@ public class AuthorServiceImpl implements AuthorService {
     }
 
     @Override
-    public Page<AuthorDTO> getAllAuthors(Integer pageNumber, Integer size) {
-        Page<Author> authors = authorRepository.findAll(PageRequest.of(pageNumber, size));
+    public Page<AuthorDTO> getAuthors(Integer offset, Integer limit) {
+        Page<Author> authors = authorRepository.findAll(PageRequest.of(offset, limit));
+        LOGGER.info("[count=" + authors.getSize() + "] authors were found");
         return authors.map(authorConverter::fromEntityToDto);
     }
 
     @Override
-    public Optional<AuthorDTO> getAuthorById(@NotBlank Integer id) {// @TODO: add log.info() here
+    public Optional<AuthorDTO> getAuthorById(Integer id) {// @TODO: 1.manage the argument validation
         Optional<Author> author = Optional.ofNullable(authorRepository.findById(String.valueOf(id))
-                .orElseThrow(() -> new java.util.NoSuchElementException(String.format("Author with ID %s not found", (id)))));
-        //log.info("Author with [id="+id+"] was found");
+                .orElseThrow(() -> new java.util.NoSuchElementException("Author with ID " + id + " not found")));
+        LOGGER.info("Author with [id=" + id + "] was found");
         return author.stream().map(authorConverter::fromEntityToDto).findFirst();
     }
 
     @Override
-    public Author addAuthor(@Valid AuthorDTO authorDTO) {
+    public AuthorDTO addAuthor(@Valid AuthorDTO authorDTO) {
         if (authorRepository.existsAuthorByNameAndLnameAndSname(authorDTO.getName(), authorDTO.getLname(), authorDTO.getSname())) {
-            throw new DuplicateKeyException("Author already exists");
+            String errorMsg = "Author with " + authorDTO.toString() + " that you are trying to add already exists";
+            LOGGER.error(errorMsg);
+            throw new DuplicateKeyException(errorMsg);
         }
-        Author saveAuthor = authorConverter.fromDtoToEntity(authorDTO); // @TODO: after end of the testing change return type to AuthorDTO and in Controller
-
-        return authorRepository.save(saveAuthor);
+        Author saveAuthor = authorConverter.fromDtoToEntity(authorDTO);
+        authorRepository.save(saveAuthor);
+        LOGGER.info("Author was saved");
+        return authorConverter.fromEntityToDto(saveAuthor);
     }
 
     @Override
-    public void deleteAuthorById(Integer id) {
+    public void deleteAuthorById(Integer id) { //@TODO: 1.manage argument validation
+        //@TODO: 2. delete data related to the author from book author also
         Optional<Author> author = authorRepository.findById(String.valueOf(id));
         if (author.isEmpty()) {
-            throw new NoSuchElementException(String.format("Author with ID %s not found", (id)));
+            String errorMsg = "Author with [ID=" + id + "] does not exist";
+            LOGGER.error(errorMsg);
+            throw new NoSuchElementException(errorMsg);
         }
         authorRepository.deleteById(String.valueOf(id));
+        LOGGER.info("Author with [id=" + id + "] was deleted");
     }
 
 }
