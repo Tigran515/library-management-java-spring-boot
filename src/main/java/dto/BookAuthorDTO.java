06f1cce8fcdb837e464cbdad648335ebfa17b50d
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-05-20 09:06:04 +0400

    💚 ci(validation):  add validation annotation

--- src/main/java/dto/BookAuthorDTO.java
+++ src/main/java/dto/BookAuthorDTO.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookAuthorDTO { //@TODO: is validation needed here ?
+    @NotNull(message = "The bookDto in BookAuthorDTO is null")
     private BookDTO bookDTO;
+    @NotNull(message = "The authorDTO in BookAuthorDTO is null")
     private AuthorDTO authorDTO;
 }
