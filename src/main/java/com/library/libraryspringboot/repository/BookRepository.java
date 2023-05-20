510badbaad738f40072f15ac54e0219db584351c
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-05-20 09:32:05 +0400

    💚 ci(method): add method

--- src/main/java/com/library/libraryspringboot/repository/BookRepository.java
+++ src/main/java/com/library/libraryspringboot/repository/BookRepository.java
@@ -3,13 +3,12 @@ package com.library.libraryspringboot.repository;
 import com.library.libraryspringboot.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, String> {
     Page<Book> findAll(final Pageable pageable);
-    boolean existsBookByTitleAndISBN(String title, String isbn);
+    boolean existsBookByISBN(String isbn);
     Optional<Book> findBookByISBN(String isbn);
 }
