50369ad2608f6c9d72a9a02e0cbedeba6c6a2aba
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-05-20 08:55:57 +0400

    ♻ refactor(fields): change validation

--- src/main/java/dto/AuthorDTO.java
+++ src/main/java/dto/AuthorDTO.java
@@ -2,30 +2,39 @@ package dto;
 
 import com.library.libraryspringboot.entity.Author;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Null;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class AuthorDTO  { // added Serializable for bytecode conv.
+    @Null(message = "The ID field must be null in the POST request")
+    private String id;
+
     @NotBlank(message = "Name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must only contain alphabetic characters")
+//    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must only contain alphabetic characters")
     private String name;
     @NotBlank(message = "Last Name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Lastname must only contain alphabetic characters")
+//    @Pattern(regexp = "^[a-zA-Z]+$", message = "Lastname must only contain alphabetic characters")
     private String lname;
     @NotBlank(message = "Last Name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Surname must only contain alphabetic characters")//@TODO: fix the BUG -> more than one name is not accepted
+//    @Pattern(regexp = "^[a-zA-Z]+$", message = "Surname must only contain alphabetic characters")//@TODO: fix the BUG -> more than one name is not accepted
     private String sname;
     private Integer born;
 
+    public AuthorDTO(){
+
+    }
     public AuthorDTO(Author author) {
         this.name = author.getName();
         this.lname = author.getLname();
         this.sname = author.getSname();
         this.born = author.getBorn();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[name=%s,lname=%s,sname=%s,born=%s]", name, lname, sname, born);
     }
 }
