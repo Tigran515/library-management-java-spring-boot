9be44c25c6122a1827a98f5ce6b7820c0b6babcb
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-05-20 09:18:59 +0400

    🔨refactored (addBookAuthor) change return type

--- src/main/java/com/library/libraryspringboot/service/BookAuthorService.java
+++ src/main/java/com/library/libraryspringboot/service/BookAuthorService.java
@@ -1,16 +1,14 @@
 package com.library.libraryspringboot.service;
 
-import com.library.libraryspringboot.Tool.BookAuthorId;
 import com.library.libraryspringboot.Tool.SearchCriteria;
 import com.library.libraryspringboot.entity.Author;
 import com.library.libraryspringboot.entity.Book;
-import com.library.libraryspringboot.entity.BookAuthor;
 import dto.BookAuthorDTO;
 import org.springframework.data.domain.Page;
 
 
 public interface BookAuthorService {
     <T> T findBooksAndAuthorsByCriteria(SearchCriteria detail); // advanced search method
-    Page<BookAuthorDTO> getAllBooksAndAuthors(final Integer pageNumber, final Integer size);
-    BookAuthor addBookAuthor(Book book, Author author); //@TODO change 
+    Page<BookAuthorDTO> getBooksAndAuthors(final Integer pageNumber, final Integer size);
+    BookAuthorDTO addBookAuthor(Book book, Author author);
 }
