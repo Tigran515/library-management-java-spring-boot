020e1aaa00d163a2d51d7c46076f53d922ff550d
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-05-20 09:39:53 +0400

    💚 ci(WebMvcConfigurer): configured Cors

---
+++ src/main/java/com/library/libraryspringboot/config/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.library.libraryspringboot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig implements WebMvcConfigurer {
+    @Value("${client.port}")
+    private String clientPort;
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(clientPort)
+                .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name())
+                .maxAge(86400);
+    }
+}
