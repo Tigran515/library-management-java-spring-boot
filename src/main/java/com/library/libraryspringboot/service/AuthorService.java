2ca0952392ed6dde1170319de71e91eb1cc356e5
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-05-20 08:59:16 +0400

    ♻ refactor(addAuthor): change return type

--- src/main/java/com/library/libraryspringboot/service/AuthorService.java
+++ src/main/java/com/library/libraryspringboot/service/AuthorService.java
@@ -1,13 +1,12 @@
 package com.library.libraryspringboot.service;
 
-import com.library.libraryspringboot.entity.Author;
 import dto.AuthorDTO;
 import org.springframework.data.domain.Page;
 import java.util.Optional;
 
 public interface AuthorService {
-    Page<AuthorDTO> getAllAuthors(final Integer pageNumber, final Integer size);
+    Page<AuthorDTO> getAuthors(final Integer pageNumber, final Integer size);
     Optional<AuthorDTO> getAuthorById(Integer id);
-    Author addAuthor(AuthorDTO authorDTO);
-    void deleteAuthorById( Integer id);
+    AuthorDTO addAuthor(AuthorDTO authorDTO);
+    void deleteAuthorById(Integer id);
 }
