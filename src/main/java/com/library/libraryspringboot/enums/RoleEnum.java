2b7f8ebb9e1d38b2d03852d51a0bc3815174c488
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-08-12 09:41:31 +0400

    💚 ci (user): create user role enum

---
+++ src/main/java/com/library/libraryspringboot/enums/RoleEnum.java
@@ -0,0 +1,11 @@
+package com.library.libraryspringboot.enums;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+
+public enum RoleEnum {
+    admin,
+    user
+}
