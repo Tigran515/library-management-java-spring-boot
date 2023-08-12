d3513470760e19330004167278905967452737e8
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-08-12 09:03:37 +0400

    🔨 refactored (field): update field filter

--- src/main/java/com/library/libraryspringboot/dto/AuthorDTO.java
+++ src/main/java/com/library/libraryspringboot/dto/AuthorDTO.java
@@ -1,10 +1,10 @@
-package dto;
+package com.library.libraryspringboot.dto;
 
 import com.library.libraryspringboot.entity.Author;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Null;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class AuthorDTO  {
@@ -12,14 +12,13 @@ public class AuthorDTO  {
     private String id;
 
     @NotBlank(message = "Name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must only contain alphabetic characters")
+    @Pattern(regexp = "^[^0-9]+$", message = "Name cannot contain numbers")
     private String name;
-    @NotBlank(message = "Last Name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Lastname must only contain alphabetic characters")
+    @Pattern(regexp = "^$|^[a-zA-Z]+$", message = "Lastname must only contain alphabetic characters")
     private String lname;
-    @NotBlank(message = "Last Name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Surname must only contain alphabetic characters")//@TODO: fix the BUG -> more than one name is not accepted
+    @Pattern(regexp = "^$|^[a-zA-Z]+$", message = "Surname must only contain alphabetic characters")//@TODO: fix the BUG -> more than one name is not accepted
     private String sname;
+    @Max(value = 2023,message = "Not valid number")
     private Integer born;
 
     public AuthorDTO(){
