acc20b4e5306ca6bc6c69134f65b92e15b4b0af6
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-08-12 09:04:34 +0400

    🔥 remove (unused comments)

--- src/main/java/com/library/libraryspringboot/repository/AuthorRepository.java
+++ src/main/java/com/library/libraryspringboot/repository/AuthorRepository.java
@@ -10,3 +10,10 @@ public interface AuthorRepository extends JpaRepository<Author, String> {
     Page<Author> findAll(final Pageable pageable);
     boolean existsAuthorByNameAndLnameAndSname(String name, String lname, String sname);
 }
+
+
+//Example of update method
+//
+//    @Modifying
+//    @Query("UPDATE YourEntity e SET e = :updatedEntity WHERE e.id = :id")
+//    void updateFieldsIgnoreId(@Param("id") Long id, @Param("updatedEntity") YourEntity updatedEntity);
