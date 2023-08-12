3296929bef5ae314627e5456513d92cf572401bc
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-08-12 09:27:39 +0400

    🔥 ci (addBook) change return type

--- src/main/java/com/library/libraryspringboot/service/BookService.java
+++ src/main/java/com/library/libraryspringboot/service/BookService.java
@@ -1,17 +1,19 @@
 package com.library.libraryspringboot.service;
 
-import dto.BookAuthorDTO;
-import dto.BookDTO;
+import com.library.libraryspringboot.dto.BookAuthorDTO;
+import com.library.libraryspringboot.dto.BookDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookService {
     Page<BookDTO> getBooks(final Integer pageNumber, final Integer size); // gets all books names only for rendering in the page
     Optional<BookDTO> getBookById(@PathVariable Integer id);
-    BookAuthorDTO addBook(BookDTO bookDTO, Integer authorId);
+    List<BookAuthorDTO> addBook(BookDTO bookDTO, List<Integer> authorId);
     void deleteBookById(@PathVariable Integer id);
+
 //    BookAuthor saveBook(BookDTO book);
 //    List<BookDTO> findBooksByDetails(String title,String ISBN);
 }
