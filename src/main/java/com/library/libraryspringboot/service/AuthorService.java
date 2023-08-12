3be5896edddb897dadeed7c8b4647eb950d5045c
Tigran Karapetyan <64483229+Tigran515@users.noreply.github.com>
2023-08-12 09:05:19 +0400

    🔥 remove (unused comments)

--- src/main/java/com/library/libraryspringboot/service/AuthorService.java
+++ src/main/java/com/library/libraryspringboot/service/AuthorService.java
@@ -1,6 +1,6 @@
 package com.library.libraryspringboot.service;
 
-import dto.AuthorDTO;
+import com.library.libraryspringboot.dto.AuthorDTO;
 import org.springframework.data.domain.Page;
 import java.util.Optional;
 
